511648d2d6425a0eb6dd2dd932b200695b498d2f
029_HarshParmar <hp259369@gmail.com>
2025-10-24 18:48:30 +0530

    Add files via upload

---
+++ 107_Subtract two matrices.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() {
+    int a[10][10], b[10][10], sub[10][10];
+    int i, j, rows, cols;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    // Subtract matrices (A - B)
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            sub[i][j] = a[i][j] - b[i][j];
+        }
+    }
+    printf("\nResultant matrix after subtraction (A - B):\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", sub[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
