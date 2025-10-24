d4a55f92f8a2205ff5a8392b275cdcaedabe8b09
029_HarshParmar <hp259369@gmail.com>
2025-10-24 22:23:41 +0530

    Add files via upload

---
+++ 114_Print lower triangular matrix.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int n, i, j;
+    printf("Enter the size of the square matrix: \n");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+    // Input matrix elements
+    printf("Enter the elements of the matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    // Print lower triangular matrix
+    printf("Lower triangular matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(j <= i) {
+                printf("%d ", matrix[i][j]);
+            } else {
+                printf("0 ");  // or just skip printing
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
