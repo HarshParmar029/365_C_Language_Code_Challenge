0da89c140b06ea044997b14af57f60cfbe4f124b
029_HarshParmar <hp259369@gmail.com>
2025-10-25 12:25:04 +0530

    Add files via upload

---
+++ 122_Print full rectangle of stars with given size.c
@@ -0,0 +1,24 @@
+/*Outer loop → for each row
+
+Inner loop → prints * for each column
+
+After one row → printf("\n"); to move to next line
+*/
+
+#include <stdio.h>
+int main() {
+    int rows, cols, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    for(i = 1; i <= rows; i++) {
+        for(j = 1; j <= cols; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
