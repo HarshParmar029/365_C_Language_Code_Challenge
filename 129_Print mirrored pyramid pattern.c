7623976910965c41f3caff0e83e27996ceacc04f
029_HarshParmar <hp259369@gmail.com>
2025-10-25 20:21:44 +0530

    Add files via upload

---
+++ 129_Print mirrored pyramid pattern.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int rows, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    for(i = 1; i <= rows; i++) {
+        // Print spaces first
+        for(j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        // Print stars
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
