8a14e43b210617826b7925b45b2e688fbd899845
029_HarshParmar <hp259369@gmail.com>
2025-10-27 01:25:37 +0530

    Add files via upload

---
+++ 164_Print all perfect numbers between 1–1000.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int num, i, sum;
+
+    printf("Perfect numbers between 1 and 1000 are:\n");
+
+    for (num = 1; num <= 1000; num++) {
+        sum = 0;
+        for (i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == num) {
+            printf("%d\n", num);
+        }
+    }
+    return 0;
+}
+
