e817c65d7c1b354750029013f49645f3777b9e19
029_HarshParmar <hp259369@gmail.com>
2025-10-27 21:18:36 +0530

    Add files via upload

---
+++ 180_Find sum of cubes of digits of a number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int num, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    while (num != 0) {
+        digit = num % 10;          // extract last digit
+        sum += digit * digit * digit;  // add cube of digit
+        num /= 10;                 // remove last digit
+    }
+    printf("Sum of cubes of digits = %d\n", sum);
+    return 0;
+}
